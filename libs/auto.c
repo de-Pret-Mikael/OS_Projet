05a036013b8c246eec109f90f7edcb8fa6e50772
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-11-26 14:57:59 +0100

    update projet

--- libs/auto.c
+++ libs/auto.c
@@ -5,42 +5,45 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-const ID_CAR[20] = {44, 77, 16, 5, 33, 23, 55, 4, 3, 31, 10, 26, 11, 18, 7, 99, 20, 8, 63, 6};
-const secteur[3] = {"S1","S2","S3"};
+const int NBR_CAR = 20;
+const int ID_CAR[20] = {
+        44, 77, 16, 5, 33, 23, 55, 4, 3, 31, 10, 26, 11, 18, 7, 99, 20, 8, 63, 6};
+const char SECTEUR[3][2] = {"S1", "S2", "S3"};
 
-typedef struct voiture{
-    int id = 0;
-    float timeS1 = 0;
-    float bestTimeS1 = INFINITY;
-    float timeS2 = 0;
-    float bestTimeS2 = INFINITY;
-    float timeS3 = 0;
-    float bestTimeS3 = INFINITY;
-    float totTime = timeS1 + timeS2 + timeS3;
-    float bestTimeSect = 0;
-    char stand = "F";
-    char out = "F";
+typedef struct voiture car;
+struct voiture {
+    int id;//= 0;
+    float timeS1;//= 0;
+    float bestTimeS1;//= INFINITY;
+    float timeS2;//= 0;
+    float bestTimeS2;//= INFINITY;
+    float timeS3;//= 0;
+    float bestTimeS3;//= INFINITY;
+    float totTime;//= timeS1 + timeS2 + timeS3;
+    char stand;//"F";
+    char out;//"F";
+} tableauVoiture[20];
 
-    void upDateTimeS1(float timeS1, float bestTimeS1){
-
+void init() {
+    for (int i = 0; i < NBR_CAR; i++) {
+        car obj = {ID_CAR[i], 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        tableauVoiture[i] = obj;
     }
-/*
-    int upDateTimeS2(){
+}
 
-    }
-
-    int upDateTimeS3(){
-
-    }
-
-    int conserve(){
-        if (stand != "F" || out != "F"){
-
+void lap(int id) {
+    struct voiture voitureSelected;
+    for(int i = 0; i < NBR_CAR;i++){
+        struct voiture obj = tableauVoiture[i];
+        if (obj.id == id){
+            voitureSelected = obj;
+            break;
         }
-    };
-    */
-}voiture;
+    }
 
-int main(int argc, char** argv){
-    printf("%f\n", upDateTimeS1(10.4, 2.5))
+}
+
+int main(int argc, char **argv) {
+    init();
+    lap(16);
 }
