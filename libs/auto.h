23bc63cf151b0345e7cd2b0f4eb7f77d3d405ccc
cyrilGrandjean <64021617+cyrilGrandjean@users.noreply.github.com>
2020-12-03 11:55:57 +0100

    update

--- libs/auto.h
+++ libs/auto.h
@@ -2,3 +2,23 @@
 // Created by patris on 26/11/20.
 //
 #pragma once
+const int NBR_CAR = 20;
+const int ID_CAR[20] = {
+        44, 77, 16, 5, 33, 23, 55, 4, 3, 31, 10, 26, 11, 18, 7, 99, 20, 8, 63, 6};
+const char SECTEUR[3][2] = {"S1", "S2", "S3"};
+int tour = 1;
+typedef struct voiture car;
+struct voiture {
+    int numero;
+    float timeS1;//= 0;
+    float bestTimeS1;//= INFINITY;
+    float timeS2;//= 0;
+    float bestTimeS2;//= INFINITY;
+    float timeS3;//= 0;
+    float bestTimeS3;//= INFINITY;
+    float tourTime;//= timeS1 + timeS2 + timeS3;
+    float bestTourTime;
+    float totalTime;
+    char stand;//"F";
+    char out;//"F";
+} tableauVoiture[20];
--- libs/cli.c
+++ libs/cli.c
@@ -1,5 +1,7 @@
 //
 // Created by Cyril Grandjean on 03-12-20.
 //
-
+#include "auto.h"
 //voiture   S1   S2   S3   tour   gap   stands
+printf("voiture   S1   S2   S3   tour   gap   stands\n");
+printf("%2d %7.3f %7.3f %7.3f %7.3f %+7.3f\n",);
